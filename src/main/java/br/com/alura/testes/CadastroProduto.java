62ddb61173bdb14d852ecf9f3a3e53398bfbb580
Fabíola <fabiolasoufer@gmail.com>
2022-12-20 21:31:35 -0300

    @fabiolasoufer Task: Ciclo de vida de uma entidade

--- src/main/java/br/com/alura/testes/CadastroProduto.java
+++ src/main/java/br/com/alura/testes/CadastroProduto.java
@@ -17,15 +17,20 @@ public class CadastroProduto {
         Produto celular = new Produto("Xiaomi Redmi", "Prefiro a Samsung", new BigDecimal("800"), celulares);
 
         EntityManager em = JPAUtil.getEntityManager();
-        ProdutoDao produtoDao = new ProdutoDao(em);
-        CategoriaDao categoriaDao = new CategoriaDao(em);
 
         em.getTransaction().begin();
 
-        categoriaDao.cadastrar(celulares);
-        produtoDao.cadastrar(celular);
+        em.persist(celulares);
+        celulares.setNome("XPTO");
 
-        em.getTransaction().commit();
-        em.close();
+        em.flush();
+        em.clear();
+
+        celulares = em.merge(celulares);
+        celulares.setNome("1234");
+        em.flush();
+        em.clear();
+        em.remove(celulares);
+        em.flush();;
     }
 }
