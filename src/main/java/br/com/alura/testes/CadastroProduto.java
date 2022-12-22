6a93c3fb76f9651c80d90992c71c2bf0ab7975c1
Fabíola <fabiolasoufer@gmail.com>
2022-12-21 22:19:59 -0300

    @fabiolasoufer Task: Consultas com JPQL

--- src/main/java/br/com/alura/testes/CadastroProduto.java
+++ src/main/java/br/com/alura/testes/CadastroProduto.java
@@ -8,29 +8,40 @@ import br.com.alura.util.JPAUtil;
 
 import javax.persistence.EntityManager;
 import java.math.BigDecimal;
+import java.util.List;
 
 public class CadastroProduto {
 
     public static void main(String[] args) {
 
+        cadastrarProduto();
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDao produtoDao = new ProdutoDao(em);
+
+        Produto p = produtoDao.buscarPorId(1l);
+        System.out.println(p.getPreco());
+
+        List<Produto> todos = produtoDao.buscarPorNomeDaCategoria("CELULARES");
+        todos.forEach(p2 -> System.out.println(p.getNome()));
+
+        BigDecimal precoDoProduto =produtoDao.buscarPrecoDoProdutoComNome("Xiaomi Redmi");
+        System.out.println("Preço do produto R$ " + precoDoProduto);
+    }
+
+    private static void cadastrarProduto() {
         Categoria celulares = new Categoria("CELULARES");
         Produto celular = new Produto("Xiaomi Redmi", "Prefiro a Samsung", new BigDecimal("800"), celulares);
 
         EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDao produtoDao = new ProdutoDao(em);
+        CategoriaDao categoriaDao = new CategoriaDao(em);
 
         em.getTransaction().begin();
 
-        em.persist(celulares);
-        celulares.setNome("XPTO");
+        categoriaDao.cadastrar(celulares);
+        produtoDao.cadastrar(celular);
 
-        em.flush();
-        em.clear();
-
-        celulares = em.merge(celulares);
-        celulares.setNome("1234");
-        em.flush();
-        em.clear();
-        em.remove(celulares);
-        em.flush();;
+        em.getTransaction().commit();
+        em.close();
     }
 }
